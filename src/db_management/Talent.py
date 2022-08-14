c0d79ef6947dc59eca8efba77069fe7a24e41524
Alena Kropacheva <jeykolton@gmail.com>
2022-08-14 17:22:19 +0300

    changed talent class

--- src/db_management/Talent.py
+++ src/db_management/Talent.py
@@ -1,20 +1,52 @@
 from utils.hash_func import get_name_hash
+from utils.lang_operations import EN, RU, get_template
 
 
 class Talent:
-    def __init__(self, lang, name: str, max, checks, desc, checks_desc=None, table_link=None, example=None, spec=None, name_hash=None):
-        self.lang = lang
-        self.name = name
-        self.max = max
-        self.checks = checks
-        self.desc = desc
-        self.checks_desk = checks_desc
-        self.table_link = table_link
-        self.example = example
-        self.spec = spec
-        if name_hash is None:
-            self.name_hash = get_name_hash(self.name)
-        else:
-            self.name_hash = name_hash
+    def __init__(self):
+        self.name_hash = None
+        self.name = get_template()
+        self.max_picked = get_template()
+        self.checks = get_template()
+        self.desc = get_template()
+        self.checks_desk = get_template()
+        self.table_link = get_template()
+        self.example = get_template()
+        self.spec = get_template()
+        self.meta = get_template()
 
+    def set_lang_version(self, lang, name, max_picked, desc, checks=None, checks_desc=None,
+                         table_link=None, example=None, spec=None, meta=None):
+        if self.name_hash is None:
+            self.name_hash = get_name_hash(name)
+        self.name.update({lang: name})
+        self.max_picked[lang] = max_picked
+        self.desc[lang] = desc
+        self.checks[lang] = checks
+        self.checks_desk[lang] = checks_desc
+        self.table_link[lang] = table_link
+        self.example[lang] = example
+        self.spec[lang] = spec
+        self.meta[lang] = meta
+        print(self.__dict__)
 
+    def add_talent_by_lang(self, rules_db, lang):
+        args = (
+            self.name_hash,
+            self.name[lang],
+            self.max_picked[lang],
+            self.checks[lang],
+            self.checks_desk[lang],
+            self.desc[lang],
+            self.table_link[lang],
+            self.example[lang],
+            self.spec[lang]
+        )
+        num_of_args = '?, ' * (len(args) - 1)
+        sql_add_talent = f""" INSERT INTO talents_{lang} VALUES ({num_of_args}?)"""
+        rules_db.execute(sql_add_talent, args)
+        rules_db.commit()
+
+    def add_talent(self, rules_db):
+        for lang in (EN, RU):
+            self.add_talent_by_lang(rules_db, lang)
