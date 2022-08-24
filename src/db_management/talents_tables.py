5108d12c8be7c230ac06c864310e2bc9150dacb8
Alena Kropacheva <jeykolton@gmail.com>
2022-08-24 16:28:20 +0300

    хехе

--- src/db_management/talents_tables.py
+++ src/db_management/talents_tables.py
@@ -7,7 +7,7 @@ from src.db_management.utils.lang_operations import EN, RU
 if __name__ == "__main__":
     new_talent = Talent()
     new_talent.set_lang_version(EN, 'nibba', 1, 'just strong back', 'athletics')
-    new_talent.set_lang_version(RU, 'васян', 1, 'просто сильная спина', 'атлетика')
+    new_talent.set_lang_version(RU, 'васян хехе', 1, 'просто сильная спина', 'атлетика')
     with sqlite3.connect(DATABASE_PATH) as rules_db:
         new_talent.add_talent(rules_db)
 
